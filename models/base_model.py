237c3a976d3e6070629878d89aec134b1cd69709
gabirle <5722@holbertonstudents.com>
2023-02-23 14:43:51 -0800

    implementation of *args and **kwargs in class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,28 @@ from datetime import datetime
 class BaseModel:
     """class BaseModel"""
 
-    def __init__(self):
-        """__init__ method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """__init__ method
+        Args:
+            *args: no_keyword argument
+            **kwargs:keyword argument
+        """
+        if len(kwargs) > 0 and kwargs is not None:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """string representation of an object
         Return: String representation of an object
         """
-        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """method to updates 'updated_at' with current datetime"""
@@ -32,7 +43,7 @@ class BaseModel:
             dictionary with all key/value of __dict__ of instance
         """
         dictionary = dict(self.__dict__)
-        dictionary["__class__"] = type(self).__name__
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         return (dictionary)
