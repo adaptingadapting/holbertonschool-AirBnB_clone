77c988258a97a687118bb5fa93a3342f6086071c
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-02-24 20:50:29 -0300

    hotfix

--- models/base_model.py
+++ models/base_model.py
@@ -23,10 +23,10 @@ class BaseModel:
                         val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, val)
         else:
-            storage.new(self)
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """string representation of an object
