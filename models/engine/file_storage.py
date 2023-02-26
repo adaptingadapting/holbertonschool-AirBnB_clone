232bfe21ecf004c2e0ff3582127c1a1e1107f828
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-02-26 17:08:54 -0300

    hotifX

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,7 @@ class FileStorage:
 
         new_dict = {}
         for key, value in self.__objects.items():
-            new_dict[key] = value.to_dict()
+            new_dict.update({key: value.to_dict()})
         with open(self.__file_path, "w") as f:
             json.dump(new_dict, f)
 
